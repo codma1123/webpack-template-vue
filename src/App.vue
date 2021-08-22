9e7481a92d8fb87e293c96cbf5bfe59b5dde7906
codma <ruddud1123@naver.com>
2021-08-22 15:55:43 +0900

    eslint 1

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <h1>{{ count }}</h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 2
+    }
+  },
+  beforeCreate(){
+    console.log('beforeCreate!', )
+  },
+  created(){
+    console.log("created!", this.count)
+  },
+  beforeMount(){
+    console.log("Before Mount!")
+    console.log(document.querySelector('h1'))
+  },
+  mounted(){
+    console.log("Mounted!")
+    console.log(document.querySelector('h1'))
+    this.count = 1;
+    
+  }
+}
+
+</script>
